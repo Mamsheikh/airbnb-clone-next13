95c28667848049623c84a6d892810047344f10f6
Munir Ali Mohammed <mamsheikh123@gmail.com>
2023-04-10 21:33:07 +0100

    feat: modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import { Nunito } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/navbar/Navbar';
+import Modal from './components/modals/Modal';
+import RegisterModal from './components/modals/RegisterModal';
+import ToastProvider from './providers/ToasterProvider';
 
 export const metadata = {
   title: 'Airbnb clone',
@@ -20,6 +23,8 @@ export default function RootLayout({
     <html lang='en'>
       <body className={font.className}>
         <Navbar />
+        <ToastProvider />
+        <RegisterModal />
         {children}
       </body>
     </html>
