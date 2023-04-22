a3f0a1c1939adb9c55be2181bb39609ed1b71a91
Munir Ali Mohammed <mamsheikh123@gmail.com>
2023-04-22 21:13:27 +0100

    feat: Listing route

---
+++ src/app/api/listings/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from 'next/server';
+
+import prisma from '@/app/libs/prismadb';
+import getCurrentUser from '@/app/actions/getCurrentUser';
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return NextResponse.error();
+
+  const body = await request.json();
+
+  const {
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    guestCount,
+    price,
+    bathroomCount,
+    location,
+  } = body;
+
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      guestCount,
+      bathroomCount,
+      price: parseInt(price, 10),
+      locationValue: location.value,
+      userId: currentUser.id,
+    },
+  });
+
+  return NextResponse.json(listing);
+}
