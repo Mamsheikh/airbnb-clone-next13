9f9490b5951ee00a5ef6f394087970975b88fffd
Munir Ali Mohammed <mamsheikh123@gmail.com>
2023-04-23 17:04:13 +0100

    feat: image slider component

---
+++ src/app/components/Carousel.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { Carousel } from 'react-responsive-carousel';
+import HeartButton from './HeartButton';
+import { SafeUser } from '../types';
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+type SliderProps = {
+  images: string[];
+  id: string;
+  currentUser?: SafeUser | null;
+};
+
+const Slider: React.FC<SliderProps> = ({ id, images, currentUser }) => {
+  const router = useRouter();
+  return (
+    <Carousel infiniteLoop autoPlay showArrows showThumbs showStatus={false}>
+      {images.map((img, i) => (
+        <div
+          key={i}
+          className='aspect-square w-full relative overflow-hidden rounded-xl'
+        >
+          <Image
+            fill
+            alt='Listing'
+            src={img}
+            className='object-cover h-full w-full group-hover:scale-110 transition'
+          />
+          <div className='absolute top-3 right-3'>
+            <HeartButton listingId={id} currentUser={currentUser} />
+          </div>
+        </div>
+      ))}
+    </Carousel>
+  );
+};
+export default Slider;
