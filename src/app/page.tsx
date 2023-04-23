40685720d63aa57bbdbdb60c09aceab07e3b5beb
Munir Ali Mohammed <mamsheikh123@gmail.com>
2023-04-23 17:28:46 +0100

    feat: listing card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
+import getListings from './actions/getListings';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
+import ListingCard from './components/listings/ListingCard';
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings();
 
-  if (isEmpty) {
+  if (listings.length === 0) {
     return <EmptyState showReset />;
   }
   return (
     <Container>
       <div className='pt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-        <div>My future listings</div>
+        {/* <div> */}
+        {listings.map((listing) => (
+          <ListingCard key={listing.id} data={listing} />
+        ))}
+        {/* </div> */}
       </div>
     </Container>
   );
