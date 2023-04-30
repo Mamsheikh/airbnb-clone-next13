2bd398b1bb4a95efdc0f555e77d7c1ded0e0b01d
Munir Ali Mohammed <mamsheikh123@gmail.com>
2023-04-30 15:20:05 -0700

    fix: index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,49 @@
+import Container from '@/app/components/Container';
+import ListingCard from '@/app/components/listings/ListingCard';
 import EmptyState from '@/app/components/EmptyState';
 
+import getListings, { IListingParams } from '@/app/actions/getListings';
 import getCurrentUser from '@/app/actions/getCurrentUser';
-import getListings from '@/app/actions/getListings';
-import PropertiesClient from './properties/PropertiesClient';
 
-const PropertiesPage = async () => {
+export const dynamic = 'force-dynamic';
+
+interface HomeProps {
+  searchParams: IListingParams;
+}
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
-  if (!currentUser) {
-    return <EmptyState title='Unauthorized' subtitle='Please login' />;
-  }
-
-  const listings = await getListings({ userId: currentUser.id });
-
   if (listings.length === 0) {
-    return (
-      <EmptyState
-        title='No properties found'
-        subtitle='Looks like you have no properties.'
-      />
-    );
+    return <EmptyState showReset />;
   }
 
-  return <PropertiesClient listings={listings} currentUser={currentUser} />;
+  return (
+    <Container>
+      <div
+        className='
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          '
+      >
+        {listings.map((listing: any) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
+  );
 };
 
-export default PropertiesPage;
+export default Home;
