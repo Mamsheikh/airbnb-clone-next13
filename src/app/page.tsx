d16e39c25dcd528ff3b10c6e39c3969a938bc75a
Munir Ali Mohammed <mamsheikh123@gmail.com>
2023-04-30 10:06:19 -0700

    feat: modify getListings with userId params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import getCurrentUser from './actions/getCurrentUser';
-import getListings from './actions/getListings';
+import getListings, { IListingParams } from './actions/getListings';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
 import ListingCard from './components/listings/ListingCard';
 
-export default async function Home() {
-  const listings = await getListings();
+interface IHomeProps {
+  searchParams: IListingParams;
+}
+
+const Home = async ({ searchParams }: IHomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
@@ -27,4 +31,6 @@ export default async function Home() {
       </div>
     </Container>
   );
-}
+};
+
+export default Home;
