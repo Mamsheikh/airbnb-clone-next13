0a0f0690683f48b888f9ed3cf12edcda9824bb3d
Munir Ali Mohammed <mamsheikh123@gmail.com>
2023-05-01 05:20:51 -0700

    feat: show correct labels on search nav

--- src/app/components/navbar/Search.tsx
+++ src/app/components/navbar/Search.tsx
@@ -1,8 +1,54 @@
 'use client';
-import useSearchModal from '@/app/hooks/useSearchModal';
+
+import { useSearchParams } from 'next/navigation';
 import { BiSearch } from 'react-icons/bi';
+
+import useSearchModal from '@/app/hooks/useSearchModal';
+import useContries from '@/app/hooks/useCountries';
+import { useMemo } from 'react';
+import { differenceInDays } from 'date-fns';
+
 const Search = () => {
   const searchModal = useSearchModal();
+  const params = useSearchParams();
+  const { getByValue } = useContries();
+
+  const locationValue = params?.get('locationValue');
+  const startDate = params?.get('startDate');
+  const endDate = params?.get('endDate');
+  const guestCount = params?.get('guestCount');
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+
+    return 'Anywhere';
+  }, [getByValue, locationValue]);
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate);
+      const end = new Date(endDate);
+      let diffTime = differenceInDays(end, start);
+
+      if (diffTime === 0) {
+        diffTime = 1;
+      }
+      return `${diffTime} Days`;
+    }
+
+    return 'Any Week';
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+
+    return 'Add Guests';
+  }, [guestCount]);
+
   return (
     <div
       onClick={searchModal.onOpen}
@@ -33,7 +79,7 @@ const Search = () => {
         px-6
         '
         >
-          Anywhere
+          {locationLabel}
         </div>
         <div
           className='
@@ -47,7 +93,7 @@ const Search = () => {
         text-center
         '
         >
-          Any Week
+          {durationLabel}
         </div>
         <div
           className='
@@ -61,7 +107,7 @@ const Search = () => {
         gap-3
         '
         >
-          <div className='hidden sm:block'>Add Guests</div>
+          <div className='hidden sm:block'>{guestLabel}</div>
           <div
             className='
             p-2
