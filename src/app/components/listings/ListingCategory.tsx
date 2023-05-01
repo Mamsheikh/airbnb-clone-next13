2586f127ff225dcf65e40f60951cd4c8236af03b
Munir Ali Mohammed <mamsheikh123@gmail.com>
2023-05-01 04:43:58 -0700

    fix: typo in listing category

--- src/app/components/listings/ListingCategory.tsx
+++ src/app/components/listings/ListingCategory.tsx
@@ -14,7 +14,7 @@ const ListingCategory: React.FC<ListingCategoryProps> = ({
 }) => {
   return (
     <div className='flex flex-col gap-6'>
-      <div className='flex flex-row items-center-gap-4'>
+      <div className='flex flex-row items-center gap-4'>
         <Icon size={40} className='text-neutral-400' />
         <div className='flex flex-col'>
           <div className='text-lg font-semibold'>{label}</div>
